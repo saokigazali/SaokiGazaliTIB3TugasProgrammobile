f69bd8cfa2755d460fa785b5f0a4ded06f3e055a
saokigazali <saokigazali2020@gmail.com>
2020-11-20 19:58:41 +0800

    tugas pemograman mobile saoki gazali tib semester tiga

--- app/src/main/java/simple/example/hewanpedia/model/Anjing.java
+++ app/src/main/java/simple/example/hewanpedia/model/Anjing.java
@@ -5,6 +5,7 @@ public class Anjing extends Hewan{
     public Anjing(String ras,String jenis, String asal, String deskripsi, int drawableRes) {
         super(ras,jenis,asal,deskripsi,drawableRes);
 
+
     }
 
 }
